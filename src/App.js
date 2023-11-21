50c93941ec2ab87ed18c7f11352584523dcc0d59
Peter Naimoli <pnaimoli@users.noreply.github.com>
2023-11-20 18:28:11 -0600

    Basic measure bar tracking committed.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import sheetMusicLogo from './sheet-music-logo.png';
 
@@ -8,9 +8,19 @@ pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker
 
 const App = () => {
   const [images, setImages] = useState([]);
-
   const [isMetronomeOn, setIsMetronomeOn] = useState(false);
   const [bpm, setBpm] = useState(60); // Default BPM
+  const [opencvReady, setOpencvReady] = useState(false);
+
+  useEffect(() => {
+    // Check if OpenCV has loaded
+    if (window.cv && window.cv.imread) {
+      setOpencvReady(true);
+    } else {
+      // If not, we listen for a custom event that indicates OpenCV has loaded
+      document.addEventListener('opencv-ready', () => setOpencvReady(true), { once: true });
+    }
+  }, []);
 
   const handleFileUpload = async (event) => {
     const file = event.target.files[0];
@@ -18,6 +28,16 @@ const App = () => {
       try {
         const pngImages = await convertPdfToPng(file);
         setImages(pngImages);
+        if (opencvReady) {
+          // Once OpenCV is ready, process each image
+          pngImages.forEach((dataUrl, index) => {
+            const img = new Image();
+            img.onload = () => processSheetMusic(img, index);
+            img.src = dataUrl;
+          });
+        } else {
+            console.error("OpenCV is not ready");
+        }
       } catch (error) {
         console.error("Error converting PDF to PNG: ", error);
       }
@@ -56,7 +76,10 @@ const App = () => {
       </header>
       <div>
         {images.map((imgSrc, index) => (
-          <img key={index} src={imgSrc} alt={`page-${index + 1}`} />
+          <div key={index}>
+            <img src={imgSrc} alt={`Page ${index + 1}`} style={{ display: 'none' }} />
+            <canvas id={`canvas-${index}`}></canvas>
+          </div>
         ))}
       </div>
       {/* Rest of your component */}
@@ -106,5 +129,99 @@ const convertPdfToPng = async (file) => {
     fileReader.readAsArrayBuffer(file);
   });
 };
+
+//const processSheetMusic = (imageElement, index) => {
+//  // Ensure OpenCV has been loaded
+//  if (window.cv && window.cv.imread) {
+//    // Read the image from the HTMLImageElement
+//    let src = window.cv.imread(imageElement);
+//    let dst = new window.cv.Mat();
+//    let gray = new window.cv.Mat();
+//    let edges = new window.cv.Mat();
+//    
+//    // Convert to grayscale
+//    window.cv.cvtColor(src, gray, window.cv.COLOR_RGBA2GRAY, 0);
+//    
+//    // Use Canny edge detector to find edges in the image
+//    window.cv.Canny(gray, edges, 50, 150, 3);
+//    
+//    // Find the lines in the edge detected image
+//    let lines = new window.cv.Mat();
+//    window.cv.HoughLinesP(edges, lines, 1, Math.PI / 180, 2, 15, 5);
+//    
+//    // Draw the lines on the source image
+//    for (let i = 0; i < lines.rows; ++i) {
+//      let startPoint = new window.cv.Point(lines.data32S[i * 4], lines.data32S[i * 4 + 1]);
+//      let endPoint = new window.cv.Point(lines.data32S[i * 4 + 2], lines.data32S[i * 4 + 3]);
+//      // Draw red lines
+//      window.cv.line(src, startPoint, endPoint, [255, 0, 0, 255], 2, window.cv.LINE_AA, 0);
+//    }
+//    
+//    // Create a canvas to show the result
+//    window.cv.imshow(`canvas-${index}`, src);
+//    
+//    // Cleanup the memory
+//    src.delete(); dst.delete(); gray.delete(); edges.delete(); lines.delete();
+//  } else {
+//    console.error('Please make sure OpenCV.js is loaded before calling this function.');
+//  }
+//};
+
+const processSheetMusic = (imageElement, index) => {
+  // Ensure OpenCV has been loaded
+  if (window.cv && window.cv.imread) {
+    // Read the image from the HTMLImageElement
+    let src = window.cv.imread(imageElement);
+    let gray = new window.cv.Mat();
+    let edges = new window.cv.Mat();
+    
+    // Convert to grayscale
+    window.cv.cvtColor(src, gray, window.cv.COLOR_RGBA2GRAY, 0);
+    
+    // Use adaptive thresholding to get a binary image
+    window.cv.adaptiveThreshold(gray, gray, 255, window.cv.ADAPTIVE_THRESH_GAUSSIAN_C, window.cv.THRESH_BINARY, 11, 2);
+    window.cv.bitwise_not(gray, gray);
+    
+    // Detect vertical lines using morphological operations
+    let verticalKernel = window.cv.getStructuringElement(window.cv.MORPH_RECT, new window.cv.Size(1, 25));
+    window.cv.morphologyEx(
+      gray,
+      edges,
+      window.cv.MORPH_OPEN,
+      verticalKernel,
+      new window.cv.Point(-1, -1),
+      2
+    );
+    
+    // Find contours of the detected lines
+    let contours = new window.cv.MatVector();
+    let hierarchy = new window.cv.Mat();
+    window.cv.findContours(edges, contours, hierarchy, window.cv.RETR_EXTERNAL, window.cv.CHAIN_APPROX_SIMPLE);
+
+    // Filter out contours that are too short or too long to be measure bars
+    let filteredContours = [];
+    for (let i = 0; i < contours.size(); ++i) {
+      let rect = window.cv.boundingRect(contours.get(i));
+      if (rect.height > 50 && rect.height < 1000) {
+        filteredContours.push(contours.get(i));
+      }
+    }
+    
+    // Draw red lines on the source image
+    for (let i = 0; i < filteredContours.length; ++i) {
+      let rect = window.cv.boundingRect(filteredContours[i]);
+      window.cv.rectangle(src, new window.cv.Point(rect.x, rect.y), new window.cv.Point(rect.x + rect.width, rect.y + rect.height), [255, 0, 0, 255], 2, window.cv.LINE_AA, 0);
+    }
+
+    // Create a canvas to show the result
+    window.cv.imshow(`canvas-${index}`, src);
+    
+    // Cleanup the memory
+    src.delete(); gray.delete(); edges.delete();
+  } else {
+    console.error('Please make sure OpenCV.js is loaded before calling this function.');
+  }
+};
+
 
 export default App;
