1fb3f143018ca61bd133b6b640ac9f2fd69fe7a1
Peter Naimoli <pnaimoli@users.noreply.github.com>
2023-12-06 02:53:25 -0600

    Remove wasm code references, service workers, etc...

--- client/src/index.js
+++ client/src/index.js
@@ -15,10 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-if ('serviceWorker' in navigator) {
-  const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
-  navigator.serviceWorker.register(swUrl)
-    .then(registration => console.log('Service Worker registered'))
-    .catch(err => console.error('Service Worker registration failed:', err));
-}
