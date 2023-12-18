b6b848e491fc705b157bfbc5b396d0c4e4629851
Peter Naimoli <pnaimoli@users.noreply.github.com>
2023-12-17 19:47:39 -0700

    Fix pylint issues and return 404 for weird paths.

--- server/measure_hider_modeler.py
+++ server/measure_hider_modeler.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, jsonify, send_from_directory
+"""Module for Flask app to process images using YOLOv8."""
+
 import base64
 import io
 import os
-#import numpy as np
+from flask import Flask, request, jsonify, send_from_directory
 from PIL import Image
 from ultralytics import YOLO
 
@@ -11,16 +12,45 @@ app = Flask(__name__, static_folder='client')
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
+    """
+    Serve files from the static folder or 'index.html' for the root path.
+
+    If the root path '/' is accessed, 'index.html' is returned. If a specific path is
+    requested, the function checks if the file exists in the static folder. If the file
+    exists, it is returned; otherwise, a 404 error is raised.
+
+    Args:
+    path (str): The requested path.
+
+    Returns:
+    Response: A Flask response object to serve the requested file or 'index.html' for the
+              root path. Returns a 404 error response if the file does not exist.
+    """
+    if path == "":
+        # Return 'index.html' for the root path
         return send_from_directory(app.static_folder, 'index.html')
+    if os.path.exists(os.path.join(app.static_folder, path)):
+        # Return the requested file if it exists
+        return send_from_directory(app.static_folder, path)
+    # Return a 404 error if the file does not exist
+    return "Not Found", 404
 
 @app.route('/process-image', methods=['POST'])
 def process_image():
+    """
+    Process an image sent in the request and predict bounding boxes using YOLO model.
+
+    The function expects a POST request with a JSON body containing a base64-encoded
+    image data URL. It decodes the image, uses the YOLO model to predict bounding boxes,
+    and returns these coordinates in a JSON format.
+
+    Returns:
+    Response: A Flask response object containing JSON data with the bounding box
+              coordinates for each detected measure in the image.
+    """
     # Extract the image data URL from the request
     image_data_url = request.json.get('imageData')
-    header, encoded = image_data_url.split(",", 1)
+    _, encoded = image_data_url.split(",", 1) # _ = header
     image_data = base64.b64decode(encoded)
     image = Image.open(io.BytesIO(image_data))
 
@@ -32,10 +62,11 @@ def process_image():
     # Convert the tensor to a list for JSON serialization
     predictions = []
     for box in results.boxes.xyxy.cpu().numpy():
-        x, y, x1, y1 = map(float, box[:4]) # Extract bounding box coordinates
-        predictions.append({"x": x, "y": y, "w": x1-x, "h": y1-y})
+        left_x, top_y, right_x, bottom_y = map(float, box[:4])
+        predictions.append({"x": left_x, "y": top_y, "w": right_x-left_x, "h": bottom_y-top_y})
 
     return jsonify(predictions)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Airplay Receiver is using localhost:5000 for whatever reason.
+    app.run('localhost', 4999, debug=True)
--- server/test_measure_hider_modeler.py
+++ server/test_measure_hider_modeler.py
@@ -1,21 +1,52 @@
+"""
+Unit tests for the Measure Hider Modeler Flask application.
+
+This module contains unit tests for testing the functionality of
+the Measure Hider Modeler Flask application, particularly focusing
+on the image processing capabilities.
+"""
+
 import unittest
 import json
 from measure_hider_modeler import app
 
 class MeasureHiderModelerTestCase(unittest.TestCase):
+    """
+    Test case for the Measure Hider Modeler Flask application.
+
+    This class implements unittest.TestCase and contains tests for
+    verifying the functionality of the Measure Hider Modeler Flask
+    application, including the processing of images.
+    """
 
     def setUp(self):
+        """
+        Set up test client for Flask application.
+
+        Initializes a test client for the Flask application and sets
+        the testing flag to True.
+        """
         self.app = app.test_client()
         self.app.testing = True
 
     def test_process_image(self):
+        """
+        Test the image processing endpoint of the Flask application.
+
+        This test verifies that the /process-image route correctly processes
+        an image provided in the request. It checks the status code of the
+        response and the length of the data returned to ensure correct
+        functionality.
+        """
         # Load image data from the file
-        with open('test_image.data', 'r') as file:
+        with open('test_image.data', 'r', encoding='utf-8') as file:
             image_data = file.read()
 
         # Example test for the /process-image route
         test_data = {'imageData': image_data}  # replace with your test data URL
-        response = self.app.post('/process-image', data=json.dumps(test_data), content_type='application/json')
+        response = self.app.post('/process-image',
+                                 data=json.dumps(test_data),
+                                 content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
         # Additional assertions to verify the response data can be added here
